c2dc7e99628d07745b377d468e9064055f528131
rama <rama@javan.co.id>
2023-01-03 18:10:03 +0700

    personal style

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,20 +1,20 @@
-import type { LoaderArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import type { LoaderArgs } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import { Form, Link, NavLink, Outlet, useLoaderData } from '@remix-run/react'
 
-import { requireUserId } from "~/session.server";
-import { useUser } from "~/utils";
-import { getNoteListItems } from "~/models/note.server";
+import { requireUserId } from '~/session.server'
+import { useUser } from '~/utils'
+import { getNoteListItems } from '~/models/note.server'
 
 export async function loader({ request }: LoaderArgs) {
-  const userId = await requireUserId(request);
-  const noteListItems = await getNoteListItems({ userId });
-  return json({ noteListItems });
+  const userId = await requireUserId(request)
+  const noteListItems = await getNoteListItems({ userId })
+  return json({ noteListItems })
 }
 
 export default function NotesPage() {
-  const data = useLoaderData<typeof loader>();
-  const user = useUser();
+  const data = useLoaderData<typeof loader>()
+  const user = useUser()
 
   return (
     <div className="flex h-full min-h-screen flex-col">
@@ -49,7 +49,7 @@ export default function NotesPage() {
                 <li key={note.id}>
                   <NavLink
                     className={({ isActive }) =>
-                      `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`
+                      `block border-b p-4 text-xl ${isActive ? 'bg-white' : ''}`
                     }
                     to={note.id}
                   >
@@ -66,5 +66,5 @@ export default function NotesPage() {
         </div>
       </main>
     </div>
-  );
+  )
 }
